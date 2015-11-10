306515cce416658020eb15920846f6fe1be8485d
ansela <ashley@Ashleys-MBP.home>
2015-11-09 22:28:49 -0500

    Add routing

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -8,18 +8,28 @@ app.config([
 			.state('home', {
 				url: '/home',
 				templateUrl: '/home.html',
-				controller: 'MainCtrl'
+				controller: 'MainCtrl',
+				resolve: {
+					postPromise: ['posts', function(posts) {
+						return posts.getAll();
+					}]
+				}
 			})
 			.state('posts', {
 				url: '/posts/{id}',
 				templateUrl: '/posts.html',
-				controller: 'PostsCtrl'
+				controller: 'PostsCtrl',
+				resolve: {
+					post: ['$stateParams', 'posts', function($stateParams, posts) {
+						return posts.get($stateParams.id);
+					}]
+				}
 			});
 		$urlRouterProvider.otherwise('home');
 	}
 ]);
 
-app.factory('posts', [function() {
+app.factory('posts', ['$http', function($http) {
 	var o = {
 		posts: [
 			{title: 'post 1', upvotes: 5},
@@ -28,6 +38,27 @@ app.factory('posts', [function() {
 			{title: 'post 4', upvotes: 9},
 			{title: 'post 5', upvotes: 4},
 		]
+	};
+	o.getAll = function() {
+		return $http.get('/posts').success(function(data) {
+			angular.copy(data, o.posts);
+		});
+	};
+	o.create = function(post) {
+		return $http.post('/posts', post).success(function(data) {
+			o.posts.push(data);
+		});
+	};
+	o.upvote = function(post) {
+		return $http.put('/posts/' + post._id + '/upvote')
+			.success(function(data) {
+				post.upvotes++;
+			});
+	};
+	o.get = function(id) {
+		return $http.get('/posts/' + id).then(function(res) {
+			return res.data;
+		});
 	};
 	return o;
 }]);
@@ -42,33 +73,30 @@ app.controller('MainCtrl', [
 		
 		$scope.addPost = function() {
 			if(!$scope.title || $scope.title === '') return;
-			$scope.posts.push({
-				title: $scope.title, 
-				link: $scope.link,
-				upvotes: 0,
-				comments: [
-					{ author: 'Ash', body: 'Cool post!', upvotes: 0 },
-					{ author: 'Jason', body: 'Love youuuu', upvotes: 0 }
-				]});
+			posts.create({
+				title: $scope.title,
+				link: $scope.link
+			});
 			$scope.title = '';
 			$scope.link = '';
 		};
 		
 		$scope.incrementUpvotes = function(post) {
-			post.upvotes++;
+			posts.upvote(post);
 		};
 	}
 ]);
 
 app.controller('PostsCtrl', [
 	'$scope',
-	'$stateParams',
 	'posts',
-	function($scope, $stateParams, posts) {
-		$scope.post = posts.posts[$stateParams.id];
+	'post',
+	function($scope, posts, post) {
+		$scope.post = post;
 		
 		$scope.addComment = function() {
 			if($scope.body === '') return;
+			if($scope.post.comments == null) $scope.post.comments = [];
 			$scope.post.comments.push({
 				body: $scope.body,
 				author: 'user',
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,98 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
+
+// get the list of posts from the db
+router.get('/posts', function(req, res, next) {
+  Post.find(function(err, posts) {
+    if(err) return next(err);
+    
+    res.json(posts);
+  });
+});
+
+// add a new post to the db
+router.post('/posts', function(req, res, next) {
+  var post = new Post(req.body);
+  
+  post.save(function(err, post) {
+    if(err) return next(err);
+    
+    res.json(post);
+  });
+});
+
+// this is used in multiple places to get a specific post based on id
+// this will be called whenever we have a route with :post, then it will
+// go into that route
+router.param('post', function(req, res, next, id) {
+  var query = Post.findById(id);
+  
+  query.exec(function(err, post) {
+    if(err) { return next(err); }
+    if(!post) {return next(new Error('can\'t find post')); }
+    
+    req.post = post;
+    return next();
+  });
+});
+
+// this was actually handled by the middleware (that param function above)
+// so all we have to do here is attach the post json to the result
+router.get('/posts/:post', function(req, res) {
+  req.post.populate('comments', function(err, post) {
+    if(err) return next(err);
+    
+    res.json(req.post);
+  });
+});
+
+router.put('/posts/:post/upvote', function(req, res, next) {
+  req.post.upvote(function(err, post) {
+    if(err) return next(err);
+    
+    res.json(post);
+  });
+});
+
+router.post('/posts/:post/comments', function(req, res, next) {
+  var comment = new Comment(req.body);
+  comment.post = req.post;
+  
+  comment.save(function(err, comment) {
+    if(err) return next(err);
+    
+    req.post.comments.push(comment);
+    req.post.save(function(err, post) {
+      if(err) return next(err);
+      
+      res.json(comment);
+    });
+  });
+});
+
+// this is used in multiple places to get a specific post based on id
+router.param('comment', function(req, res, next, id) {
+  var query = Comment.findById(id);
+  
+  query.exec(function(err, comment) {
+    if(err) { return next(err); }
+    if(!comment) return next(new Error('can\'t find comment')); 
+    
+    req.comment = comment;
+    return next();
+  });
+});
+
+router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+  req.comment.upvote(function(err, comment) {
+    if(err) return next(err);
+    
+    res.json(comment);
+  })
 });
 
 module.exports = router;
